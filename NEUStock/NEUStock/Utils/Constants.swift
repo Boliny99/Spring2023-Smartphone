55153dd688a820791e70dada8fc7c7f3b9741db6
Boliny99 <yang.bol@northeastern.edu>
2023-02-19 17:59:07 -0800

    "add initial code for stocks Feb 11"

---
+++ NEUStock/NEUStock/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  NEUStock
+//
+//  Created by Bolin Yang on 2/19/23.
+//
+
+import Foundation
+
+let apiKey = "a0f8dbc02569706d12c1e1c197dd05cc"
+let baseURL = "https://financialmodelingprep.com/api/v3/quote-short/"
---
+++ NEUStock/NEUStock/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  NEUStock
+//
+//  Created by Bolin Yang on 2/19/23.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import SwiftSpinner
+
+class ViewController: UIViewController {
+    @IBOutlet weak var txtStockSymbol: UITextField!
+    
+    @IBOutlet weak var lblStockPrice: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func getStockPrice(_ sender: Any) {
+        guard let symbol = txtStockSymbol.text else {return}
+        let url = "\(baseURL)\(symbol)?apikey=\(apiKey)"
+        print("URL = \(url)")
+        
+        SwiftSpinner.show("Getting stock price for \(symbol)")
+        
+        Alamofire.request(url).responseJSON { response in
+            SwiftSpinner.hide()
+            if response.error != nil {
+                print("Error in response")
+                return
+            }
+            guard let rawData = response.data else {return}
+            guard let jsonArray = JSON(rawData).array else {return}
+            guard let stock = jsonArray.first else {return}
+            print(stock)
+            let symbol = stock["symbol"].stringValue
+            let price = stock["price"].doubleValue
+            let volume = stock["volume"].intValue
+            
+            print("Symbol = \(symbol)")
+            print("price = \(price)")
+            print("volume = \(volume)")
+            self.lblStockPrice.text = "Price = \(price) $"
+            
+        }
+        
+    }
+    
+}
+
