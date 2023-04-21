336f00d43bfe0873368cfacb9d5a2fea4c7e8913
Boliny99 <yang.bol@northeastern.edu>
2023-04-21 07:33:09 -0700

    checking code for localization and add app icon

---
+++ LocalizationExample/LocalizationExample/Utilities/LocalizationUtil.swift
@@ -0,0 +1,10 @@
+//
+//  LocalizationUtil.swift
+//  LocalizationExample
+//
+//  Created by Bolin Yang on 4/21/23.
+//
+
+import Foundation
+
+var strHelloWorld = NSLocalizedString("hello_world", comment: "This is hello world string")
---
+++ LocalizationExample/LocalizationExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LocalizationExample
+//
+//  Created by Bolin Yang on 4/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblHelloWorld: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblHelloWorld.text = strHelloWorld
+        
+    }
+
+
+}
+
